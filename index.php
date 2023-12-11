ab3dc64b697412e30ef8db419e78ff8e692582c1
Matt Vanderpol <matt@vanderpol.net>
2023-12-11 09:26:19 -0800

    Support for better default post styling

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 
 <div class="page-wrapper">
 
-	<?php if ( have_posts() ) : ?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-
-		<?php endwhile; ?>
-
-	<?php endif; ?>
-
+    <?php 
+        if ( have_posts() ) {
+		    while ( have_posts() ) {
+			    the_post();
+                $post_format = get_post_format();
+                if ( $post_format ) {
+                    get_template_part( 'template-parts/content', $post_format );
+                } else {
+                    get_template_part( 'template-parts/content', get_post_type() );
+                }
+            }
+        }
+    ?>
 </div>
 
 <?php
